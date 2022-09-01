3521a0055a1d93cf31f4274f2811a0b3d88c2e7b
Saygat <aleksandr.filimonov@sequenia.com>
2022-09-02 00:29:29 +0700

    Add new examples

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] array = {1,12,31,4,15,12,17,18};
+
+int n = array.Length;
+int find = 12;
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
